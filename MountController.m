77b9f9f0a3068babe80756d57c6ceb11e41179ab
Iliya Lindov <iliya@lindov.com>
2014-01-01 22:47:12 +0200

    First working release. May be considered rev. 0.1, although it's not tagget as such yet. :)

--- MountController.m
+++ MountController.m
@@ -10,32 +10,59 @@
 
 @implementation MountController
 
-NSString *_diskFilePath = @"/Users/ilia/Desktop/test.sparsebundle";
+NSDictionary *usersDataStructure;
 
 - (void) awakeFromNib{
-    [statusField setObjectValue:@"Initialized..."];
     [usersList removeAllItems];
-    [usersList addItemWithTitle:@"Iliya Lindov"];
-    [usersList addItemWithTitle:@"Petya Lindova"];
+    
+    NSDictionary *configuration = [[NSDictionary alloc] initWithContentsOfFile:@"/etc/PreLoginMount.plist"];
+    // NSLog(@"%@\n", configuration);
+    
+    usersDataStructure = [configuration objectForKey:@"Users"];
+    NSArray *userNames = [usersDataStructure allKeys];
+    
+    // TODO: Fail if array elements are 0
+    
+    for(NSString *name in userNames){
+        [usersList addItemWithTitle:name];
+    }
+    
+    [statusField setObjectValue:@"Initialized..."];
 }
 
 - (IBAction)mountRequested:(id)sender{
+    // TODO: sanitize the password value - escape special chars
     [statusField setObjectValue:@"Attempting to mount..."];
-    BOOL result = [self attemptToMount:_diskFilePath withPassword:[diskUnlockPassword objectValue]];
+    
+    NSString *diskFilePath = [[usersDataStructure objectForKey:[usersList titleOfSelectedItem]] objectForKey:@"DiskImage"];
+    
+    BOOL result = [self attemptToMount:diskFilePath withPassword:[diskUnlockPassword objectValue]];
     if (result){
         [NSApp terminate:self];
+    }
+    else {
+        // TODO: sleep 3s.
+        [statusField setObjectValue:@"Mount failed..."];
     }
 }
 
 - (BOOL)attemptToMount:(NSString *)diskFilePath
           withPassword:(NSString *)password {
-    NSLog(@"Path: '%@'; Password: '%@'\n", diskFilePath, password);
+    // NSLog(@"Path: '%@'; Password: '%@'\n", diskFilePath, password);
+    NSString *mountCommand = [NSString stringWithFormat:@"/usr/bin/printf '%@' | /usr/bin/hdiutil attach -stdinpass %@", password, diskFilePath];
+    
+    // NSLog(@"Command: %@\n", mountCommand);
+    
     @try {
+        
         NSTask *execution = [[NSTask alloc] init];
-        [execution setLaunchPath:@"/bin/echo"];
-        [execution setArguments:[[NSArray alloc] initWithObjects:@"'123' >/tmp/777.txt", nil]];
+        [execution setLaunchPath:@"/bin/bash"];
+        [execution setArguments:[[NSArray alloc] initWithObjects:@"-c", mountCommand, nil]];
         [execution launch];
         [execution waitUntilExit];
+        // NSLog(@"Status: %d\n", [execution terminationStatus]);
+        if (![execution terminationStatus])
+            return YES;
     }
     @catch (NSException *exception) {
         NSLog(@"Exception - %@", [exception reason]);
